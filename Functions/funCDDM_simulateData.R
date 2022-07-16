933ebd28331c2b1a224c4b9f6d65085458563b3b
Adriana Chávez <adrifelcha@gmail.com>
2022-07-16 09:16:01 -0700

    Fix: Cleaning code and creating an unifying function that returns all relevant output: randomWalk, final coordinates, RT, choice degree and choice radians

--- Functions/funCDDM_simulateData.R
+++ Functions/funCDDM_simulateData.R
@@ -5,7 +5,9 @@
 ###################################################
 ###########################   by Adriana F. Chavez
 
-myCDDMsimData.randomWalk <- function(trials, mu.1, mu.2, sigma.driftrate, thresh){
+myCDDMsimData.randomWalk <- function(trials, mu.1, mu.2, std.driftrate, thresh){
+    s.init <- c(0,0) 
+    iter <- 1000
     state <- array(NA, dim = c(iter, 2, trials))   #States are saved in a 3dimensional array
     finalT <- NA #An additional empty vector to store RT (a.k.a. total number of iterations)
     
@@ -13,8 +15,8 @@ myCDDMsimData.randomWalk <- function(trials, mu.1, mu.2, sigma.driftrate, thresh
         state[1,,a] <- s.init   # States are stored in 3D array
       
         for(t in 2:iter){
-            d1 <- rnorm(1,mu1.drift,sigma.driftrate)
-            d2 <- rnorm(1,mu2.drift,sigma.driftrate)
+            d1 <- rnorm(1,mu1.drift,std.driftrate)
+            d2 <- rnorm(1,mu2.drift,std.driftrate)
             state[t,,a] <- state[t-1,,a]+c(d1,d2)
             pass <- sqrt(sum(state[t,,a]^2))
         
@@ -24,7 +26,7 @@ myCDDMsimData.randomWalk <- function(trials, mu.1, mu.2, sigma.driftrate, thresh
             }
         }
       
-        if(pass> thresh){
+        if(pass > thresh){
           A <- state[t-1,,a];   B <- state[t,,a]
           last.step.x <- seq(A[1],B[1],length.out = 100)
           last.step.y <- seq(A[2],B[2],length.out = 100)
@@ -35,24 +37,44 @@ myCDDMsimData.randomWalk <- function(trials, mu.1, mu.2, sigma.driftrate, thresh
               if(round(pass2,1) == thresh){
                  circunf <- c(last.step.x[i],last.step.y[i])
                  break
-              }
+                }
           }
-    
           state[t,,a] <- circunf
         }
     }
+    
+    output <- list(state,finalT)
+    names(output) <- c("state","RT")
+    return(output)
 }
 
 myCDDMsimData.getCoordinates <- function(randomWalk){
-   end <- nrow(randomWalk)
-   coord <- randomWalk[end,,]
+   K <- nrow(randomWalk)
+   I <- dim(randomWalk)[3]
+   
+   coord <- matrix(NA, ncol=2,nrow=I)
+         for(i in 1:I){
+               for(k in 1:K){
+                      if(!is.na(randomWalk[k,1,i])){
+                           a <- k
+                      }else{
+                        break
+                      }
+               }
+             coord[i,] <- randomWalk[a,,i]
+         }
+   
    return(coord)
 }
 
-myCDDMsimData.getDegrees <-  function(x,y){   
+
+myCDDMsimData.getDegrees <-  function(coord){
+    x <- coord[,1]
+    y <- coord[,2]
     theta <- atan2(y,x) * 180 / pi  #Angle with respect of y=0
-        if(theta<0){
-          theta <- theta+360   #Correction for whole circle (360)
+        if(sum(theta<0)>0){
+          theta.0 <- which(theta<0)
+          theta[theta.0] <- theta[theta.0]+360   #Correction for whole circle (360)
           }   
     return(theta)
 }
@@ -63,12 +85,16 @@ myCDDMsimData.getRadians <- function(theta.d){
 }
 
 
-myCDDMsimData.simData <- function(trials, mu.1, mu.2, sigma.driftrate, thresh){
-      randomWalk <-  myCDDMsimData.randomWalk(trials,mu.1,mu.2,sigma.driftrate,thresh)
-      coord <-
-      degrees <-
-      radians <-
-        
-        
+myCDDMsimData.simData <- function(trials, mu.1, mu.2, std.driftrate, thresh){
+      randomWalk <-  myCDDMsimData.randomWalk(trials,mu.1,mu.2,std.driftrate,thresh)
+      RT <- randomWalk$RT
+      randomWalk <- randomWalk$state
+      coord <- myCDDMsimData.getCoordinates(randomWalk)
+      degrees <- myCDDMsimData.getDegrees(coord)
+      radians <- myCDDMsimData.getRadians(degrees)
       
+      output <- list(randomWalk,coord,degrees,radians)
+      names(output) <- c("randomWalk","coord","degrees","radians")
+      
+      return(output)
 }
