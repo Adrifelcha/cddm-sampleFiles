514599299921895cdf99fb4efa23e61ec1759e44
Adriana Chavez <achavezd@uci.edu>
2022-08-23 19:11:25 -0700

    Small fixes after first attempt

--- toyExample.R
+++ toyExample.R
@@ -10,18 +10,18 @@ n.burnin = 0
 n.thin = 1
 
 X <- datos[,1:10]
-N <- length(X)
+N <- ncol(X)
 
 modelFile <- "cddm.bug"
     write('
             model{
                   # Likelihood
                     for (i in 1:N) {
-                         X[1:2,] ~ dcddm(drift, bound, ter0, theta0)
+                         X[1:2,i] ~ dcddm(drift, bound, ter0, theta0)
                     }
                       
                   # Priors
-                    drift ~ dnorm(0,1)
+                    drift ~ dnorm(0,1)T(0,)
                     bound ~ dunif(0,1000)
                     ter0 ~ dexp(1)
                     theta0 ~ dunif(0,6.283185)
@@ -31,6 +31,7 @@ modelFile <- "cddm.bug"
 data <- list("X","N")
 parameters <- c("drift", "bound", "ter0", "theta0")
 
-samples <- jags(data, parameters, model.file=model, n.chains=n.chains,
+fileName <- "toyExample_samples.RData"
+samples <- jags(data=data, parameters.to.save=parameters, model=modelFile, n.chains=n.chains,
                 n.iter=n.iter, n.burnin=n.burnin, n.thin=n.thin, DIC=T)
 save(samples,file=fileName)
