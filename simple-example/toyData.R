223b5c71cb8435e3dfde97164908e0b030ab017d
Adriana Chavez <achavezd@uci.edu>
2022-08-31 00:12:50 -0700

    Remove plotting function, to only load data

--- simple-example/toyData.R
+++ simple-example/toyData.R
@@ -4,23 +4,22 @@ source("../Functions/fun_simData.R")
 
 # Parameter values used in the simulation
 trials = 200
-thresh=2.45
+true.bound = 2.45
 mu1 <- 1.5
 mu2 <- 1.25
-ndt <- 0.1
-drift.Angle <- cddm.getVectorAngle(mu1,mu2)
-drift.Length <- cddm.getVectorLength(mu1,mu2)
+true.ndt <- 0.1
+true.drift.Angle <- cddm.getVectorAngle(mu1,mu2)
+true.drift.Length <- cddm.getVectorLength(mu1,mu2)
 
 # Evaluate if a data file already exists
 searchFile <- "./toyData.csv"
       #If file does not exist, we simulate data and write a file
       if(!file.exists(searchFile)){
-         simulateData <- cdd.simData(trials,drift.Angle,drift.Length,thresh,ndt)
+         simulateData <- cdd.simData(trials,true.drift.Angle,true.drift.Length,true.bound,true.ndt)
          write.csv(simulateData,"./toyData.csv",row.names = FALSE)
       }
 
 # Read data
-y <- read.csv("./toyData.csv")    
+datos <- read.csv("./toyData.csv")    
 # Print and plot the data
-head(y,200)
-cddm.plotData(y)
+cddm.plotData(datos)
